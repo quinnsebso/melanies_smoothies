dc3fb3c747481633eedcefc11af289767c0c3e6e
quinnsebso <quinn.sebso@onesix.ai>
2026-07-17 16:36:02 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 from snowflake.snowpark.functions import col
 import requests  
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -16,7 +17,11 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# <-- FIX 1: Added col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')) 
+
+# <-- FIX 2: Convert to Pandas DataFrame so pd_df exists
+pd_df = my_dataframe.to_pandas() 
 # st.dataframe(data=my_dataframe, width='stretch')
 # st.stop()
 
