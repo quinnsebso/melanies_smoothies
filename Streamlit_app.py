3a24092b1d670350bd862bb7b0e1da4bde5f10b4
quinnsebso <quinn.sebso@onesix.ai>
2026-07-17 16:32:54 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, width='stretch')
 # st.stop()
 
-pd_df = my_datafram.to_pandas()
+pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
